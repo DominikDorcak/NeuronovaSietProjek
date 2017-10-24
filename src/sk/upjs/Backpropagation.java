b8a78024f43e6a59defa4d84802bb6a222a8792f
domin <domin@LAPTOP-T8FQ3IB5>
2017-10-24 19:48:07 +0200

    pridana inicializacia vah

--- src/sk/upjs/Backpropagation.java
+++ src/sk/upjs/Backpropagation.java
@@ -1,11 +1,41 @@
-
 package sk.upjs;
 
 import java.util.*;
 
-
 public class Backpropagation {
-    
+
+    private double vystup;
+    private int[] popisSiete = {3, 7, 6, 5, 1};
+    private List<Double> vystupy = new ArrayList<>();
     private List<double[][]> vahy = new ArrayList<>();
-    
+
+    public void inicializuj() {
+        //inicializacia vah
+        double[][] W;
+        for (int i = 0; i < popisSiete.length - 2; i++) {
+            W = new double[popisSiete[i]][popisSiete[i + 1] - 1];
+            for (int j = 0; j < W.length; j++) {
+                for (int k = 0; k < W[j].length; k++) {
+                    W[j][k] = Math.random() * 0.5;
+                }
+
+            }
+            vahy.add(Arrays.copyOf(W, W.length));
+
+        }
+        //prechod z poslednej vnutornej na vonkajsiu vrstvu
+        W = new double[popisSiete[popisSiete.length - 2]][popisSiete[popisSiete.length - 1]];
+        for (int j = 0; j < W.length; j++) {
+            for (int k = 0; k < W[j].length; k++) {
+                W[j][k] = Math.random() * 0.5;
+            }
+        }
+        vahy.add(Arrays.copyOf(W, W.length));
+    }
+
+    public void spracujVstup(double x, double y) {
+        //TODO kod pre spracovanie jedneho vstupu z treningovej vzorky
+        
+    }
+
 }
